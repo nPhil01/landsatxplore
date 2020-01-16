77ec93e2a9ecf3aa3fc1db6452ec88e8e00ffe2f
nPhil01 <n_phil01@uni-muenster.de>
2020-01-16 15:44:08 +0100

    Bugfix: Sometimes a wrong sat-number was returned

--- landsatxplore/util.py
+++ landsatxplore/util.py
@@ -12,7 +12,12 @@ def guess_dataset(identifier):
     """Guess data set based on a scene identifier."""
     if is_product_id(identifier):
         sat = int(identifier[3])
+        if sat == 4 and identifier[2] == 'T':
+            sat = 5
     else:
         sat = int(identifier[2])
+        if sat == 4 and identifier[1] == 'T':
+            sat = 5
+
     datasets = {2: 'LANDSAT_MSS_C1', 4: 'LANDSAT_MSS_C1', 5: 'LANDSAT_TM_C1', 7: 'LANDSAT_ETM_C1', 8: 'LANDSAT_8_C1'}
     return datasets[sat]
